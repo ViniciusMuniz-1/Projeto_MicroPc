57899e63cedffa0ef3a57bb0126fd12be0291d1b
José Vinicius <vinicius.j@ifrn.edu.br>
2023-01-02 20:40:57 -0300

    transformando uma instrução em binário

--- conversor.py
+++ conversor.py
@@ -3,14 +3,13 @@
 
 arq = open("Projeto_MicroPc\intrucoes.txt")
 instruction = arq.readlines()
-string = str(instruction).strip('[]')
+string = str(instruction).strip('[]').strip("''")
 string = string.split(" ")
 for instructions in instruction:
-    if(instructions == "addi"):
-        hex = "001100"
+    if(instructions == "addi $8 $0 3"):
+        bin = "00110000100000000000000000000011"
     
 ar2 = open("Projeto_MicroPc\intructionshex.txt", "w+")
-
 for inst in instruction:
-    ar2.write(inst)
+    ar2.write(hex)
 
