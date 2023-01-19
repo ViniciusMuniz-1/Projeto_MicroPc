d1c6bee018c12902a1971f0a725d214664d0a4be
José Vinicius Targino Muniz <20222014040010@ifrn.local>
2023-01-19 15:42:43 -0300

    teste com função hex

--- conversor.py
+++ conversor.py
@@ -5,57 +5,58 @@
 #Função que possui a função de transformar um número em binário para hexadecimal. 
                  #Entrada: lista em bin com os números separados em 4 casas cada um; 
                  #Saída: lista em hex
-def bintohex(l): 
-    for num in bin:
-        if(num == "0000"):
-            num = "0"
-            numhex.append(num)
-        elif(num == "0001"):
-            num = "1"
-            numhex.append(num)
-        elif(num == "0010"):
-            num = "2"
-            numhex.append(num)
-        elif(num == "0011"):
-            num = "3"
-            numhex.append(num)
-        elif(num == "0100"):
-            num = "4"
-            numhex.append(num)
-        elif(num == "0101"):
-            num = "5"
-            numhex.append(num)
-        elif(num == "0110"):
-            num = "6"
-            numhex.append(num)
-        elif(num == "0111"):
-            num = "7"
-            numhex.append(num)
-        elif(num == "1000"):
-            num = "8"
-            numhex.append(num)
-        elif(num == "1001"):
-            num = "9"
-            numhex.append(num)
-        elif(num == "1010"):
-            num = "a"
-            numhex.append(num)
-        elif(num == "1011"):
-            num = "b"
-            numhex.append(num)
-        elif(num == "1100"):
-            num = "c"
-            numhex.append(num)
-        elif(num == "1101"):
-            num = "d"
-            numhex.append(num)
-        elif(num == "1110"):
-            num = "e"
-            numhex.append(num)
-        elif(num == "1111"):
-            num = "f"
-            numhex.append(num)
-    return numhex
+# def bintohex(l): 
+#     for num in bin:
+#         if(num == "0000"):
+#             num = "0"
+#             numhex.append(num)
+#         elif(num == "0001"):
+#             num = "1"
+#             numhex.append(num)
+#         elif(num == "0010"):
+#             num = "2"
+#             numhex.append(num)
+#         elif(num == "0011"):
+#             num = "3"
+#             numhex.append(num)
+#         elif(num == "0100"):
+#             num = "4"
+#             numhex.append(num)
+#         elif(num == "0101"):
+#             num = "5"
+#             numhex.append(num)
+#         elif(num == "0110"):
+#             num = "6"
+#             numhex.append(num)
+#         elif(num == "0111"):
+#             num = "7"
+#             numhex.append(num)
+#         elif(num == "1000"):
+#             num = "8"
+#             numhex.append(num)
+#         elif(num == "1001"):
+#             num = "9"
+#             numhex.append(num)
+#         elif(num == "1010"):
+#             num = "a"
+#             numhex.append(num)
+#         elif(num == "1011"):
+#             num = "b"
+#             numhex.append(num)
+#         elif(num == "1100"):
+#             num = "c"
+#             numhex.append(num)
+#         elif(num == "1101"):
+#             num = "d"
+#             numhex.append(num)
+#         elif(num == "1110"):
+#             num = "e"
+#             numhex.append(num)
+#         elif(num == "1111"):
+#             num = "f"
+#             numhex.append(num)
+#     return numhex
+
 
 #Bloco de código que transforma a instrução em binário
 def insttobin(instruction):
@@ -177,8 +178,9 @@ instruction = str(instruction).strip('[]').strip("''")
 #número em hexadecimal em outro arquivo
 arqhex = open("intructionshex.txt", "w+")
 numhex = []
-bintohex(numhex)
 cont = 0
 for num in bin:
     arqhex.write(numhex[cont])
     cont+=1
+ 
+hex(numhex)
