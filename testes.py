e393a666de713baaec422c5b46aa68e1834e0228
José Vinicius <vinicius.j@ifrn.edu.br>
2023-02-06 14:45:01 -0300

    Adicionado o teste para criação de interface

--- testes.py
+++ testes.py
@@ -1,12 +1,20 @@
-from tkinter import Tk
+from tkinter import *
 from tkinter.filedialog import askopenfilename
-
 janela_padrao = Tk().withdraw()
-caminho_do_arquivo = askopenfilename(filetypes = (("Arquivos de texto", "*.txt"), ("Arquivos csv", "*.csv")))
+janela = Tk()
 
-if caminho_do_arquivo:
-    with open(caminho_do_arquivo, encoding='latin_1') as arquivo:
-        for linha in arquivo:
-            print(linha, end='')
-else:
-    print("Nenhum arquivo selecionado")
+janela.title("Insira aqui o seu título")
+
+texto = Label(janela, text="Insira aqui o seu texto do botão")
+
+texto.grid(column=0, row=0, padx=10, pady=10)
+
+botao = Button(janela, text="Insira aqui a ação do seu botão", command=askopenfilename(filetypes = (("Arquivos de texto", "*.txt"), ("Arquivos csv", "*.csv"))))
+
+botao.grid(column=0, row=1, padx=10, pady=10)
+
+texto_resposta = Label(janela, text="")
+
+texto_resposta.grid(column=0, row=2, padx=10, pady=10)
+
+janela.mainloop()
